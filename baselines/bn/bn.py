20db9da8f9465dea0a23c5c1bb8ff3088b28be06
fmq <850241532@sjtu.edu.cn>
2021-01-25 12:01:28 +0800

    complete test on bn

--- baselines/bn/bn.py
+++ baselines/bn/bn.py
@@ -40,9 +40,9 @@ def train_bn(dataset, args):
 
 def train_mimic_bn(bn_args):
     print("start train")
-    df = pd.read_csv(bn_args.train_csv_path, index_col=0)
+    df = preprocess(bn_args)
     df = df.astype(int)
-    df = df.drop('row_id', axis=1)
+    df = df.drop(['row_id','icustay_id','valuenum1','valuenum2'], axis=1)
     model = bn.structure_learning.fit(df, methodtype='hc', scoretype='bic')
     model = bn.parameter_learning.fit(model, df, methodtype="bayes", verbose=2)
     with open(bn_args.model_save_path, 'wb') as f:
@@ -51,20 +51,34 @@ def train_mimic_bn(bn_args):
     
 def test_mimic_bn(bn_args):
     print("start test")
-    df = pd.read_csv(bn_args.test_csv_path, index_col=0)
+    df = preprocess(bn_args)
     df = df.astype(int)
-    df = df.drop('row_id', axis=1)
+    df = df.drop(['row_id','icustay_id','valuenum1','valuenum2'], axis=1)
     with open(bn_args.model_save_path, 'rb') as f:
         model = pickle.load(f)
-    bn.plot(model)
-    target = ['flag','valuenum1','valuenum2']
-    y = df[target]
-    x = df.drop(target, axis=1)
-    for index, x_row in x.iterrows():
-        y_hat = bn.inference.fit(model, variables=target, evidence=dict(x_row), verbose=2)
-        print(y_hat)
-        raise ValueError
-    print("test finished")
+    
+    target = ['flag']
+    y_df = df[target]
+    x_df = df.drop(target, axis=1)
+
+    acc = 0
+    y_hat_list = list()
+    y_true_list = list()
+
+    for index, x_row in x_df.iterrows():
+        factors = bn.inference.fit(model, variables=target, evidence=dict(x_row), verbose=0)
+        y_hat = factors.values[0] #probability that flag=0
+        y_hat_list.append(y_hat)
+        y_true = y_df.loc[index, target].values
+        y_true_list.append(y_true)
+        if int(y_hat) == y_true:
+            acc +=1
+    print("acc=",acc/len(y_hat_list))
+    print(y_hat_list)
+    print(y_true_list)
+    with open("test_result.pkl", 'wb') as f:
+        pickle.dump([y_hat_list,y_true_list], f)
+    print("test finished, result saved.")
 
 def get_bn_args():
     parser = argparse.ArgumentParser()
@@ -80,6 +94,35 @@ def get_bn_args():
     bn_args = parser.parse_args()
     return bn_args
 
+def preprocess(bn_args):
+    if bn_args.task == "train":
+        df = pd.read_csv(bn_args.train_csv_path)
+    else:
+        df = pd.read_csv(bn_args.test_csv_path)
+
+    df = df.astype(int)
+    cur_id = None
+    index_list = list()
+    for index, row in df.iterrows():
+        icustay_id = row['icustay_id']
+        if cur_id is None:
+            cur_id = icustay_id
+        elif cur_id != icustay_id:
+            index_list.append(index-1)
+            cur_id = icustay_id
+    df = df.iloc[index_list]
+    #print(df)
+    return df 
+
+def count_flag(df):
+    print(df['flag'].sum() / df['flag'].size)
+    #in test set: 0.34355345911949686
+    #in train set: 0.35031409501374167
+
+    #dummy test acc = 0.6564465
+    #test acc = 0.6564465
+
+
 
 if __name__ == "__main__":
     
@@ -93,3 +136,5 @@ if __name__ == "__main__":
         train_mimic_bn(bn_args)
     else:
         test_mimic_bn(bn_args)
+    #df = preprocess(bn_args)
+    #count_flag(df)
